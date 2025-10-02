f13be4a7056ea91095e60f6e2e1984dcd952c4f5
ChienDC <doancaochien.1994@gmail.com>
2025-10-02 18:37:12 +0700

    vercel

--- src/pages/CareerMatrix.tsx
+++ src/pages/CareerMatrix.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
-import { Typography } from 'antd';
-import ManagementMatrix from '../components/Matrix/ManagementMatrix';
 import SimpleMatrix from '../components/Matrix/SimpleMatrix';
-import { hasValidSupabaseConfig } from '../lib/supabase';
-
-const { Title } = Typography;
 
 const CareerMatrix: React.FC = () => {
-  // Sử dụng SimpleMatrix nếu không có Supabase config
-  if (!hasValidSupabaseConfig()) {
-    return <SimpleMatrix />;
-  }
-
-  return (
-    <div>
-      <Title level={2}>Lộ trình sự nghiệp</Title>
-      <ManagementMatrix />
-    </div>
-  );
+  return <SimpleMatrix />;
 };
 
 export default CareerMatrix;
